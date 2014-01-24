33af32d7515492448a0dda815008e3ce2a834e3f
tbpgr <tbpgr@tbpgr.jp>
2014-01-25 00:00:09 +0900

    recover Array#together_select spec

--- spec/open_classes/array_spec.rb
+++ spec/open_classes/array_spec.rb
@@ -173,6 +173,77 @@ describe Array do
     end
   end
 
+  context :together_select do
+    cases = [
+      {
+        case_no: 1,
+        case_title: 'single valid case',
+        inputs: [[1, 2, 3, 4], [4, 2, 3, 1]],
+        condition: 'first == second',
+        method_name: :together_select,
+        expected: [[2, 3], [2, 3]],
+      },
+      {
+        case_no: 2,
+        case_title: 'multi valid case',
+        inputs: [[1, 2, 3, 4], [4, 2, 3, 1]],
+        condition: '[first.odd?, second.even?]',
+        expected: [[1, 3], [4, 2]],
+        method_name: :together_select,
+      },
+      {
+        case_no: 3,
+        case_title: 'multi valid case',
+        inputs: [[1, 2, 3, 4], [4, 2, 3, 1]],
+        condition: '[first.odd?, second.even?]',
+        expected: [[1, 3], [4, 2]],
+        method_name: :tselect,
+      },
+      {
+        case_no: 4,
+        case_title: 'multi valid case',
+        inputs: [[1, 2, 3, 4], [4, 2, 3, 1]],
+        condition: '[first.odd?, second.even?]',
+        expected: [[1, 3], [4, 2]],
+        method_name: :together_find_all,
+      },
+      {
+        case_no: 5,
+        case_title: 'multi valid case',
+        inputs: [[1, 2, 3, 4], [4, 2, 3, 1]],
+        condition: '[first.odd?, second.even?]',
+        expected: [[1, 3], [4, 2]],
+        method_name: :tfindall,
+      },
+    ]
+
+    cases.each do |c|
+      it "|case_no=#{c[:case_no]}|case_title=#{c[:case_title]}" do
+        begin
+          case_before c
+
+          # -- given --
+          # nothing
+
+          # -- when/then --
+          actual = c[:inputs].method(c[:method_name]).call { |first, second| eval c[:condition], binding }
+
+          expect(actual).to eq(c[:expected])
+        ensure
+          case_after c
+        end
+      end
+
+      def case_before(c)
+        # implement each case before
+      end
+
+      def case_after(c)
+        # implement each case after
+      end
+    end
+  end
+
   context :together_reduce do
     cases = [
       {
