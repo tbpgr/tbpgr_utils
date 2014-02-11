d3b48076d2a84ed7121042d3bc0d20f97ed675b3
tbpgr <tbpgr@tbpgr.jp>
2014-02-12 04:16:32 +0900

    add coveralls settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 # encoding: utf-8
 require 'simplecov'
+require 'coveralls'
+Coveralls.wear!
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
 SimpleCov.start
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
