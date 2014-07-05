275e75e1f751721f112407d5a004adca24089a7d
tbpgr <tbpgr@tbpgr.jp>
2014-07-06 01:20:58 +0900

    Fix rubocop warning 'TrailingComma' in to_tab_heading_spec

--- spec/open_classes/string/to_tab_heading_spec.rb
+++ spec/open_classes/string/to_tab_heading_spec.rb
@@ -9,26 +9,26 @@ describe String do
         case_no: 1,
         case_title: '> case',
         input: 'hoge>hige',
-        expected: "hoge\n\thige",
+        expected: "hoge\n\thige"
       },
       {
         case_no: 2,
         case_title: '+ case',
         input: 'hoge+hige',
-        expected: "hoge\nhige",
+        expected: "hoge\nhige"
       },
       {
         case_no: 3,
         case_title: '^ case',
         input: 'hoge>hige^hege',
-        expected: "hoge\n\thige\nhege",
+        expected: "hoge\n\thige\nhege"
       },
       {
         case_no: 4,
         case_title: 'mix case',
         input: 'hoge>hige1+hige2^hege',
-        expected: "hoge\n\thige1\n\thige2\nhege",
-      },
+        expected: "hoge\n\thige1\n\thige2\nhege"
+      }
     ]
 
     cases.each do |c|
