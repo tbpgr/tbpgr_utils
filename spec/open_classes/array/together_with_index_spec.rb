7122a9aa69936b7036a615036e56aa509625b514
tbpgr <tbpgr@tbpgr.jp>
2014-07-11 00:21:26 +0900

    Fix rubocop warning 'TrailingComma' in together_with_index_spec

--- spec/open_classes/array/together_with_index_spec.rb
+++ spec/open_classes/array/together_with_index_spec.rb
@@ -9,14 +9,14 @@ describe Array do
         case_no: 1,
         case_title: 'valid case',
         inputs: [[1, 2, 3], %w(one two three)],
-        expected: ['0:1:one', '1:2:two', '2:3:three'],
+        expected: ['0:1:one', '1:2:two', '2:3:three']
       },
       {
         case_no: 2,
         case_title: 'contain nil case',
         inputs: [[1, 2, 3], %w(one two)],
-        expected: ['0:1:one', '1:2:two', '2:3:'],
-      },
+        expected: ['0:1:one', '1:2:two', '2:3:']
+      }
     ]
 
     cases.each do |c|
